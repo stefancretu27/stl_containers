f7bbbe4d303546f0495391b18ce1938616db3e4c
Stefan Cretu <stefancretu27@gmail.com>
2020-01-04 19:55:23 +0200

    added array operations

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "sequence_containers.hpp"
+
+int main()
+{
+	//sequence containers
+	array_operations();
+	
+	return 0;
+}
---
+++ sequence_containers.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+#include <array>
+#include <vector>
+#include <deque>
+#include <forward_list>
+#include <list> 
+
+//fixed size arrays
+void array_operations();
+//dynamic size arrays
+void vector_operations();
+void deque_operations();
+void fwd_list_operations();
+void list_operations();
